2dd21171fab4883296f0375a9a2d5fb1b5503e38
Malin Hanak <malinehanak@gmail.com>
2019-12-02 10:42:34 +0100

    dead code removed

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,27 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import { RegisterForm } from './RegisterForm';
 import client from '../client';
-import news from '../data/news';
-/*
-
-This is Home.js, for now this page should be responsible for displaying a
-H1 inside a main tag.
-
-TODO: Create a functional component
-  * At the time being this component takes no props.
-  * It should contain a main tag
-  * The main tag should contain a H1 with the text CTS2020.
-  * The H1 should have a data-testid of page-title.
-
-TODO: Add reference to form component
-*/
 
 export const Home = () => {
-  useEffect(() => {
-    //console.log('data:', news);
-  }, []);
-
   return (
     <main data-testid="main-content">
       <h1>CTS 2020</h1>
